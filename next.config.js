6c4b01e983d292d4359451dd9bf751d825df5619
berlin <1580940252@qq.com>
2023-05-09 14:08:36 +0800

    feat: update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+  openAnalyzer: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
-  webpack: (config) => {
-    // this will override the experiments
+  webpack: (config, { isServer }) => {
+    // Enable experimental topLevelAwait support
     config.experiments = { topLevelAwait: true };
-    // this will just update topLevelAwait property of config.experiments
-    // config.experiments.topLevelAwait = true
     return config;
   },
-};
+});
